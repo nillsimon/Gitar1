90c52b5b770a03e1d37c44d3d5a48033ad69e25e
SimonNill <nillsimon24@gmail.com>
2019-01-22 14:28:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/boss/myapplication/SettingsFragment.java
@@ -0,0 +1,12 @@
+package com.example.boss.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visializer);
+    }
+}
